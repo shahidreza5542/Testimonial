9033f5c64a77c2fa7bbe8b17aeb5cd9c817e77d4
shahidreza <shahidreza660@gmail.com>
2026-04-27 11:34:54 +0530

    sd

--- vite-project/src/components/TestmonialCard.jsx
+++ vite-project/src/components/TestmonialCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import img from '../assets/profile-thumbnail.png'
 
 const TestmonialCard = () => {
     return (
         <div className='w-screen h-screen bg-gray-50 flex justify-center items-center'>
             <div className='bg-white shadow-md px-5 py-5 border border-gray-200 rounded-2xl w-70 md:w-80 h-55 md:h-60 flex gap-5 flex-col'>
                 <div className='flex gap-2'>
-                    <img src="/src/assets/profile-thumbnail.png" alt="" className='w-10 h-10' />
+                    <img src={img} alt="" className='w-10 h-10' />
                     <div>
                         <h2 className='text-md'>Shahid Reza</h2>
                         <p className='text-sm text-gray-400'>@shahidreza</p>
